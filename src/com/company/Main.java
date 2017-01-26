52a8b3ebce8d1d6bf071b94a6ba067158d96e6d2
dskrumov <krumov_ood@abv.bg>
2017-01-26 18:28:04 +0200

    changes2
    Add the second exercise
    
    this is my test commit
    
    test commit N1
    
    "Deleted test file"

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,25 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Sumator sumator=new Sumator();
+       /* Sumator sumator=new Sumator();
         System.out.println(sumator.sum(12,12));
         System.out.println(sumator.sum(2.5,2.5));
         System.out.println(sumator.sum("a","b"));
         System.out.println(sumator.sum(123456789,123456780));
         System.out.println(sumator.sum(1.22222222222,1.2222222222));
+        System.out.println((sumator.sum(null,"b"))); */
+
+
+        BinaryTree tree=new BinaryTree(3);
+        tree.addNode(55);
+        tree.addNode(44);
+        tree.addNode(98);
+
+
+        tree.sort();
+        System.out.println(tree.search(98));
+        System.out.println(tree.search(97));
+
+
     }
 }
